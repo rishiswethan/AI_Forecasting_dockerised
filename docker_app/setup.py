4608e19c063c26992670c086c016b8770c1d001c
Rishi Swethan <rishiswethan.c.r@gmail.com>
2022-11-26 19:43:50 +0530

    Amending setup

--- docker_app/setup.py
+++ docker_app/setup.py
@@ -1,9 +1,13 @@
 import os
+import source.config as config
 
 def create_folder(new_path):
     if not os.path.exists(new_path):
         os.makedirs(new_path)
 
-create_folder("docker_app/save_files")
-create_folder("docker_app/data")
-create_folder(os.path.join("docker_app/data", "input_folder"))
+create_folder(config.INPUTS_PATH)
+create_folder(config.DATA_FOLDER)
+create_folder(config.TRAIN_DATA_FOLDER)
+create_folder(config.TEST_DATA_FOLDER)
+create_folder(config.OUTPUT_FOLDER_PATH)
+create_folder(config.SAVE_FOLDER)
